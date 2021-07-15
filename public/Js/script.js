d03114ad04875fdf77d1eed2a91bfa351bb6ebda
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-07-15 18:59:31 +0200

    change index.js with script.js in footer

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,4 +1,16 @@
 // on s'assure que le Dom est chargé
 window.onload=() => {
     // Ici le document est chargé
+fetch("https://github.com/OpenClassrooms-Student-Center/JWDP5")
+main();
 
+function main() {
+  getArticles();
+}
+
+// Récupérer les articles depuis l'API
+function getArticles() {
+  fetch(" http://localhost:3000/api/furniture")
+    .then(function (res) {
+      return res.json();
+    })
