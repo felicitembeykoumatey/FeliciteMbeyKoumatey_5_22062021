4bf2df0a3506f77a527de9707376bd79fd01624d
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-07-17 14:23:47 +0200

    move forlder on disk c

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,16 +1,15 @@
-// on s'assure que le Dom est chargé
-window.onload=() => {
-    // Ici le document est chargé
-fetch("https://github.com/OpenClassrooms-Student-Center/JWDP5")
-main();
-
-function main() {
-  getArticles();
-}
-
+//Script pour l'index
+//On initialise nos variables
+let furniture;
+let $furnitureList = document.querySelector("#furnitures-list");
 // Récupérer les articles depuis l'API
-function getArticles() {
-  fetch(" http://localhost:3000/api/furniture")
-    .then(function (res) {
-      return res.json();
-    })
+fetch("http://localhost:3000/api/furniture").then(async (result_) => {
+  //On rend asynchrone notre fonction
+  const result = await result_.json(); //Le reste du code s'execute après l'execution de la promesse
+  result.forEach((result) => {
+    furniture = result; //Result deviens furniture
+    console.log("furniture", furniture);
+    //Appel de nos functions
+    furnitureCard();
+  });
+});
