5a32c338a8bb7893e4a5f3e2e49fc78d29a1a37d
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-07-21 17:55:47 +0200

    add width , margin, padding

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,14 +1,19 @@
 fetch("http://localhost:3000/api/furniture/")
   .then((res) => res.json())
+  .catch((error) => {
+    let productsContainer = document.querySelector(".products-container");
+    productsContainer.innerHTML =
+      "Error affichage images. Lancez le serveur local au Port 3000.<br>Si le problème persiste, contactez-nous.";
+  })
   .then((array_furnitures) => {
     let output = "";
     array_furnitures.forEach(function (product) {
       output += `
         <figure class="product">
-        <img src=${product.imageUrl} class="products_container"/>
+        <img src=${product.imageUrl} class="products_container" alt="image_product"/>
         <figcaption class="description">
         <p>${product.name}</p>
-        <p>${product.description}</p>
+                <p>${product.description}</p>
         <div class="price">
         <p>${product.price},00€</p>
         </div>
