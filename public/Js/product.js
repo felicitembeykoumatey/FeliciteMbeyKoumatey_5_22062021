c9ff343af246566391f7b2cec0f6f1ee6b1ee35c
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-07-30 16:09:36 +0200

    modification code

--- public/Js/product.js
+++ public/Js/product.js
@@ -1,7 +1,3 @@
-// on s'assure que le DOM est chargé
-window.onload = () => {
-  //Exécuté après le chargement de la page
-
 //affichage du produit qui a été selectionné par l'id//
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
@@ -14,16 +10,15 @@ const productPrice = document.querySelector(".product_price");
 const varnishSelect = document.querySelector("#varnish_select");
 
 function getArticles() {
-  // On récupère uniquement le produit dont on a besoin 
-  fetch(`http://localhost:3000/api/furniture/`${id})
+  // On récupère uniquement le produit dont on a besoin
+  fetch(`http://localhost:3000/api/furniture/` + id)
     .then(function (response) {
       return response.json();
     })
-     .then(function (resultatAPI) {
- 
+    .then(function (resultatAPI) {
       article = resultatAPI;
       productName.innerHTML = article.name;
       productImg.src = article.imageUrl;
       productDescription.innerText = article.description;
-
-    };
+    });
+}
--- public/Js/script.js
+++ public/Js/script.js
@@ -1,15 +1,11 @@
-// on s'assure que le DOM est chargé
-window.onload = () => {
-  //Exécuté après le chargement de la page
+fetch("http://localhost:3000/api/furniture/")
+  .then((res) => res.json())
+  .then((array_furnitures) => {
+    let output = "";
+    console.log(array_furnitures);
 
-  fetch("http://localhost:3000/api/furniture/")
-    .then((res) => res.json())
-    .then((array_furnitures) => {
-      let output = "";
-      console.log(array_furnitures[0]);
-
-      array_furnitures.forEach(function (product) {
-        output += `
+    array_furnitures.forEach(function (product) {
+      output += `
         <figure class="product">
         <img src=${product.imageUrl} class="products_container" alt="image_product"/>
         <figcaption class="description">
@@ -20,8 +16,7 @@ window.onload = () => {
         </figcaption>
         </figure>
         `;
-      });
-      document.getElementById("products").innerHTML = output;
-    })
-    .catch((error) => console.log("Erreur : " + error));
-};
+    });
+    document.getElementById("products").innerHTML = output;
+  })
+  .catch((error) => console.log("Erreur : " + error));
