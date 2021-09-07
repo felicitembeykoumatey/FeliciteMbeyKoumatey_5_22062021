32d89687b25dcbcce92e622c89d82ca4585c5509
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-09-07 14:51:10 +0200

    html order page fait

---
+++ public/Js/order.js
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Votre commande|Orinoco</title>
+
+    <meta
+      property="og: title"
+      content="Meuble design vintage et mobilier pas cher-Orifurniture"
+    />
+    <meta property="og: type" content="website" />
+    <meta property="og: url" content="" />
+    <meta property="og: image" content="logo.webp" />
+    <meta
+      property="og: description"
+      content="Bienvenu sur la boutique Orifurniture de l'entreprise Orinoco spécialisée dans la vente en ligne des meubles en chêne. Découvrez nos meubles vintages : Table, chaisses..."
+    />
+    <meta name="Copyright" content="Orifurniture" />
+    <meta name="author" content="Mbey_Koumatey-Orifurniture web developer" />
+    <meta
+      name="description"
+      content="Bienvenu sur la boutique Orifurniture de l'entreprise Orinoco spécialisée dans la vente en ligne des meubles en chêne. Découvrez nos meubles vintages : Table, chaisses..."
+    />
+    <meta
+      name="keywords"
+      content="meuble vintage, meuble design, tables en bois, chêne, vernis, bois, chaise en bois, meuble tv"
+    />
+
+    <link
+      rel="preload"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      as="style"
+    />
+
+    <link rel="icon" type="image/png" href="favicon.ico" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    />
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+
+  <body>
+    <header class="nav">
+      <a href="../index.html">
+        <img src="../logo.webp" alt="logo de l'entreprise" />
+      </a>
+      <h1>Merci pour d'avoir achetez nos meubles!</h1>
+    </header>
+    <main></main>
+
+    <footer id="order">
+      <div class="copyright" style="text-align: center">
+        <p>Copyright © 2021 Orifurniture by Orinoco - Tous droits réservés</p>
+      </div>
+    </footer>
+    <script src="./order.js"></script>
+  </body>
+</html>
