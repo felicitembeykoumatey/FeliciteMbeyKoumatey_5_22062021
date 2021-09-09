69f5850e4e0870909439d5d2c0445be9d0c7e8da
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-09-10 00:52:29 +0200

    j'essaie de faire fonctionner addEventlistner

--- public/Js/basket.js
+++
@@ -1,3 +0,0 @@
-alert("Votre produit a été ajouté au panier");
-localStorage.getItem(id) ;
-console.log(getItem);
--- public/Js/product.js
+++ public/Js/product.js
@@ -1,19 +1,15 @@
 // récupération de la chaine de requete dans l'url
 const queryString_url_id = window.location.search;
 
-//méthode pour extraire l'id
 const urlSearchParams = new URLSearchParams(queryString_url_id);
 
 const id = urlSearchParams.get("id");
 
 const quantityElt = document.getElementById("quantity");
 
-const basketElt = document.getElementById("add_to_basket");
-
-// variables
+const basketElt = document.querySelector("button");
 
 //fonction
-
 function addToBasket() {
   productName = furniture.name;
   id = furniture._id;
@@ -32,61 +28,58 @@ fetch(`http://localhost:3000/api/furniture/${id}`)
 
     // la structure html pour l'affichage du produit selectionné
     cardArticle += ` 
-    <div id="container_page_product" class="products_container">
+    <div class ="container_page_product">
         <figure>
-            <img class="product_single"src="${furniture.imageUrl}"
+            <img  class="image"src="${furniture.imageUrl}"
        alt="meuble en chêne" />
-         </figure>
-       <div class="product">
-            <section class="meuble">
+      
+            <figcaption class="meuble">
               <h2>${furniture.name}</h2>
               <p>${furniture.description}</p>
          
-               <h3> ${furniture.price / 100} € </h3>
-               <div class="quantity_product">
-          <label for="furnitureNum">Quantité :</label>
-          <input id="furnitureNum" type="number" name="furnitureNum" value="1" min="1">
-        </div>
-          </section>
-         
-        
-            <form>
+               <span> ${furniture.price / 100} € </span>
+                <form>
+          <label for="quantity">Quantité :</label>${quantityElt.value}
+          <input id="quantity" type="number"  value="1" min="1">
+     
               <label for="option_product">vernis:</label>
-              <select name="option_product" id="option_product">
+              <select id="option_product">
                 <option value="varnish">Choix</option>
               </select>
+          <a href="./view/basket.html"><button id="add_to_basket">Ajouter au panier</button>
+             </a>
             </form>
-
-            <button id="add_to_basket" type="submit" name="add_to_basket">
-              Ajouter au panier
-            </button>
-    
+             </figcaption>
+       </figure>
     `;
-    console.log(furniture);
 
     // formulaire option
     let optionVarnish = furniture.varnish;
 
     let structureOptions = [];
     //la boucle for pour afficher toutes les options du produit
-    for (let j = 0; j < optionVarnish.length; j++) {
+    for (let varnish = 0; varnish < optionVarnish.length; varnish++) {
       structureOptions =
         structureOptions +
-        `<option value="${j + 1}">${optionVarnish[j]}</option>`;
+        `<option value="${varnish + 1}">${optionVarnish[varnish]}</option>`;
     }
     // Injection html dans la page web produit
     document.getElementById("container_page_product").innerHTML = cardArticle;
     // Injection html dans la pageproduit pour le choix vernis
 
     const varnishElt = document.getElementById("option_product");
-
     varnishElt.innerHTML = structureOptions;
 
     //Ecouter le bouton click
     basketElt.addEventListener("click", addToBasket);
-    console.log(addEventListener);
+    //------------Le localStorage--------------------------
 
     //-------Stocker la récupération des valeurs du formulaire dans le localStorage
+    //Déclaration de la variable dans laquelle on met les keys et values dans le localstorage
+    let produitEnregistreDansLocalStorage = JSON.parse(
+      localStorage.getItem("produit")
+    );
+    console.log(produitEnregistreDansLocalStorage);
 
     localStorage.setItem(id, JSON.stringify(furniture));
     console.log(localStorage);
