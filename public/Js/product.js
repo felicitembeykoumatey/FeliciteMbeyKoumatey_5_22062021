ca4524cc68c3de9d210ad3788560b8f57c4c9979
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-09-15 16:37:48 +0200

    modification sur js , html et css
    modification bd color (css), correction erreur dans les fichiers js et html product

--- public/Js/product.js
+++ public/Js/product.js
@@ -1,21 +1,7 @@
 //*-----récupération de l'id produit dans l'url avec la méthode URLSearchParams----------
-let urlSearchParams = new URLSearchParams(document.location.search);
-let id = urlSearchParams.get("id");
-let url = "http://localhost:3000/api/furniture";
-let localStorage;
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
 
-//* Affichage produit dans la page--------------------------------------------------
-let request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-  if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-    furniture = JSON.parse(this.responseText);
-    productChoice();
-  }
-};
-//-------initialisation de la requête----------------------------
-request.open("GET", url + "/" + id);
-//----- envoi de la requête----------------------------
-request.send();
 //-----------Const et variables -----------------------------
 const varnishElt = document.getElementById("varnish-select");
 console.log("varnishElt:" + varnishElt);
@@ -27,151 +13,84 @@ const priceElt = document.getElementById("prix");
 console.log("priceElt:" + priceElt);
 
 const descriptionElt = document.getElementById("description");
+console.log("descriptionElt:" + descriptionElt);
+
 const imageElt = document.getElementById("image");
+console.log("imageElt:" + imageElt);
 const quantityElt = document.getElementById("quantityProduct");
+console.log("quantityElt:" + quantityElt);
 
-productSendInLocalStorage = [];
-//-----Produit choisi---------------------------------
-function productChoice() {
-  //------Stockage données HTML dans la variable---------------
+main();
 
-  //----- Affichage des données-------------------------------
-  titleElt.textContent = furniture.name;
-  priceElt.textContent = furniture.price / 100 + " €";
-  descriptionElt.textContent = furniture.description;
-  imageElt.src = furniture.imageUrl;
+function main() {
+  getArticles();
+  addToBasket();
+}
 
-  //------Création des options vernis meubles--------------------
+function getArticles() {
+  // On récupère uniquement le produit dont on a besoin via le paramètre dans la requête
+  fetch(`http://localhost:3000/api/furniture/${id}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch((error) => {
+      let container = document.getElementById("container");
+      container.innerHTML =
+        "oups! Avez-vous bien lancé le serveur local au Port 3000 ?";
+    })
+    .then(function (furniture) {
+      // On place les données reçues via l'API aux bons endroits sur la page
+      titleElt.textContent = furniture.name;
+      priceElt.textContent = furniture.price / 100 + " €";
+      descriptionElt.textContent = furniture.description;
+      imageElt.src = furniture.imageUrl;
+      // formulaire option----------------------------------------------------------------------------------------------------------------------------------
+      let optionVarnish = furniture.varnish;
+      console.log("optionVarnish:" + optionVarnish);
 
-  // formulaire option
-  let optionVarnish = furniture.varnish;
-  console.log("optionVarnish:" + optionVarnish);
+      let structureOptions = [];
+      //la boucle for pour afficher toutes les options du produit
+      for (let varnish = 0; varnish < optionVarnish.length; varnish++) {
+        structureOptions =
+          structureOptions +
+          `<option value="${varnish + 1}">${optionVarnish[varnish]}</option>`;
+        //---Injection html dans la page produit pour le choix vernis
 
-  let structureOptions = [];
-  //la boucle for pour afficher toutes les options du produit
-  for (let varnish = 0; varnish < optionVarnish.length; varnish++) {
-    structureOptions =
-      structureOptions +
-      `<option value="${varnish + 1}">${optionVarnish[varnish]}</option>`;
-    //---Injection html dans la pageproduit pour le choix vernis
-
-    varnishElt.innerHTML = structureOptions;
-    console.log("varnishElt.innerHTML:" + varnishElt.innerHTML);
-  }
-  //------- sélection des Options---------------------------------------------------
-
-  document
-    .getElementById("varnish-select")
-    .addEventListener("change", function (e) {
-      varnishElt = e.target.value;
+        varnishElt.innerHTML = structureOptions;
+        console.log("varnishElt.innerHTML:" + varnishElt.innerHTML);
+      }
     });
+}
+//-------------Fonctions ajouter dans le panier----------------------------------
+function addToBasket() {
+  const addToBasket = document.getElementById("add-to-basket");
+  console.log("addToBasket:" + addToBasket);
 
-  console.log(varnishElt);
-  //--------sélection de la quantité---------------------------------------------------
-  document
-    .getElementById("quantityProduct")
-    .addEventListener("change", function (e) {
-      quantityElt = e.target.value;
-    });
-  console.log("quantityElt:" + quantityElt);
-  //-----------bouton ajouter au panier------------------------------------------------------
-  const addToBasket = document.getElementById("btn-ajouter");
-  addToBasket.addEventListener("click", function () {
-    console.log("helloClik");
-
+  addToBasket.addEventListener("click", () => {
+    alert("Votre article a été ajouté au panier");
     if (varnishElt != undefined && quantityElt != undefined) {
       console.log("hello if");
-      furniture.varnish = varnishElt;
-      furniture.quantity = quantityElt;
-    } else if (varnishElt == undefined && quantityElt != undefined) {
-      console.log("hello else if");
+      // ------ Création du produit qui sera ajouté au panier--------------
+      let productAdded = {
+        name: titleElt.textContent,
+        price: priceElt.textContent,
+        quantity: quantityElt.value,
+        _id: id,
+      };
+      console.log("productAdded:" + productAdded);
 
-      furniture.varnish = furniture.varnish[0];
-      furniture.quantity = quantityElt;
-    } else if (varnishElt != undefined && quantityElt == undefined) {
-      furniture.varnish = varnishElt;
-      furniture.quantity = 1;
-    } else {
-      furniture.varnish = furniture.varnish[0];
-      furniture.quantity = 1;
+      // ----------------- Gestion du localStorage
+      let arrayProductsInBasket = [];
+
+      // Si le localStorage existe, on récupère son contenu, on l'insère dans le tableau arrayProductsInCart, puis on le renvoit vers le localStorage avec le nouveau produit ajouté.
+      if (localStorage.getItem("products") !== null) {
+        arrayProductsInBasket = JSON.parse(localStorage.getItem("products"));
+
+        // Si le LS est vide, on le crée avec le produit ajouté
+      }
+      arrayProductsInBasket.push(productAdded);
+      localStorage.setItem("products", JSON.stringify(arrayProductsInBasket));
     }
-    alert("Votre article a été ajouté au panier");
-    priceTotal();
   });
-  console.log(addEventListener);
-
-  //* Fonction du prix total dans localStorage------------------------------------
-  function priceTotal() {
-    let price = parseInt(furniture.price);
-    // let prixDuPanier = JSON.parse(localStorage.getItem("prixTotal"));
-    let priceInBasket = JSON.parse(window.localStorage.getItem("priceTotal"));
-    console.log("priceInBasket");
-    if (prixDuPanier == 0) {
-      //localStorage.setItem(
-      window.localStorage.setItem(
-        "priceTotal",
-        prixDuPanier + (price / 100) * furniture.quantity
-      );
-    } else {
-      //localStorage.setItem(
-      window.localStorage.setItem(
-        "priceTotal",
-        prixDuPanier + (price / 100) * furniture.quantity
-      );
-    }
-  }
-  console.log("priceTotal:" + priceTotal);
-
-  //-------Fonction du prix total dans localStorage------------------------------------
-  function priceTotal() {
-    let price = parseInt(furniture.price);
-
-    let prixDuPanier = JSON.parse(window.localStorage.getItem("priceTotal"));
-    if (prixDuPanier == 0) {
-      //localStorage.setItem(
-      window.localStorage.setItem(
-        "priceTotal",
-        prixDuPanier + (price / 100) * furniture.quantity
-      );
-    } else {
-      //localStorage.setItem(
-      window.localStorage.setItem(
-        "priceTotal",
-        prixDuPanier + (price / 100) * furniture.quantity
-      );
-    }
-  }
-  //---------------Ajout dans localStorage------------------------------------------
-  //localStorage.setItem("article", JSON.stringify(furniture));
-  window.localStorage.setItem("article", JSON.stringify(furniture));
-  console.log(localStorage);
 }
-//--Fonction ajouter un produit selectionné dans LocalStorage
-const addProductLocalStorage = () => {
-  // localStorage.setItem("article", JSON.stringify(productSendInLocalStorage));
-  window.localStorage.setItem(
-    "article",
-    JSON.stringify(productSendInLocalStorage)
-  );
-};
-
-//-------------Stocker la recupération des valeurs du formulaire dans le local Storage
-//Déclaration de la variable "productSendInLocalStorage" dans laquelle on met les key et les valeurs qui sont dans le localStorage
-
-//let productSendInLocalStorage = JSON.parse(localStorage.getItem("article"));
-let productSendInLocalStorage = JSON.parse(
-  window.localStorage.getItem("article")
-);
-//---JSON.parse c'est pour convertir les données au format JSON qui sont dans le localStorage en objet JS.
-
-//---S'il y a déjà des produits d'enregistré dans le Local Storage
-
-if (productSendInLocalStorage) {
-  addProductLocalStorage();
-  console.log("ok");
-}
-
-// S'il n' y a pas de produit d'enregistré dans le Local Storage
-else {
-  addProductLocalStorage();
-}
+console.log("addEventlistener:" + addEventListener);
